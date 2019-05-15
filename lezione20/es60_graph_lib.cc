2439de24c8c3e8c35ddf3b80ad8c9f1146c13609
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2019-05-15 16:44:44 +0200

    scheletro lezione 20 per soluzione labirinto

--- lezione20/es60_graph_lib.cc
+++ lezione20/es60_graph_lib.cc
@@ -3,13 +3,88 @@
 \*/
 
 #include "graph_lib.hh"
+#include <fstream>
+
+using namespace std;
 
 int
 main() {
 
   try {
+    // leggo labirinto
+    ifstream file("maze1.txt");
+    vector<vector<int> > vertex_pos;
+    vector<string> rows;
+    while ( file.good() ) {
+      string line;
+      getline( file, line, '\n');
+      if ( line.size() > 0 ) {
+        rows.push_back(line);
+        cout << "len = " << line.size() << '\n';
+      }
+      cout << line << '\n';
+    }
+    file.close();
+    unsigned nrow = unsigned( rows.size() );
+    unsigned ncol = unsigned( rows[0].size() );
+    cout << "lette " << rows.size() << "righe\n";
+    // costruzione grafo
+    GraphLib::Graph G("maze");
 
+    // conto i vertici del grafo associato al labirinto
+    // e assegno numero consecutivo
+    // inizializzo mappa (i,j)-> numero del vertice
 
+    vertex_pos.resize(nrow);
+    for ( unsigned i=0; i < nrow; ++i ) {
+      vertex_pos[i].resize(ncol);
+      std::fill(vertex_pos[i].begin(), vertex_pos[i].end(), -1 );
+    }
+
+    unsigned count = 0;
+    for ( unsigned i = 0; i < nrow; ++i ) {
+      for ( unsigned j = 0; j < ncol; ++j ) {
+        char c = rows[i][j];
+        if ( c == ' ' || c == 'I' || c == 'U' ) {
+          // posizione libera, aggiungo vertice al grafo
+          vertex_pos[i][j] = count++;
+        }
+      }
+    }
+    // so quanti sono i vertici, alloco memoria corrispondente
+    G.resize_vertex( count );
+
+    // costruisco grafo
+    for ( unsigned i = 0; i < nrow; ++i ) {
+      for ( unsigned j = 0; j < ncol; ++j ) {
+        int vij = vertex_pos[i][j];
+
+        if ( vij < 0 ) continue; // muro, niente da fare
+
+        char c  = rows[i][j];
+        if ( c == 'I' ) G.setup_vertex( vij, "Ingresso" );
+        if ( c == 'U' ) G.setup_vertex( vij, "Uscita" );
+
+        // cerco i vicini
+        if ( j > 0 ) {
+          int vL = vertex_pos[i][j-1];
+          if ( vL >= 0 ) G.add_edge( vij, vL );
+        }
+        if ( j < ncol-1 ) {
+          int vR = vertex_pos[i][j+1];
+          if ( vR >= 0 ) G.add_edge( vij, vR );
+        }
+        if ( i > 0 ) {
+          int vT = vertex_pos[i-1][j];
+          if ( vT >= 0 ) G.add_edge( vij, vT );
+        }
+        if ( i < nrow-1 ) {
+          int vB = vertex_pos[i+1][j];
+          if ( vB >= 0 ) G.add_edge( vij, vB );
+        }
+      }
+    }
+    G.dump(cout);
   }
   catch ( string const & error ) {
     cerr << "Errore: " << error << "\n\n";
--- lezione20/graph_lib.hh
+++ lezione20/graph_lib.hh
@@ -6,7 +6,7 @@
 #include <iomanip>   // maniopolazione dell I/O esemio setw, precision
 #include <sstream>   // lettura/scrittura su stringa
 #include <utility>   // STL pair
-#include <set>       // STL set object
+#include <vector>    // STL vector object
 #include <algorithm> // STD std::transform
 #include <string>    // STL string
 #include <cmath>     // sin, cos funzioni matematiche di base
@@ -15,54 +15,113 @@
 
 namespace GraphLib {
 
+  using namespace std;
+
   class Vertex;
   class Vertex_of_a_tree;
   class Edge;
   class Graph;
   class Tree;
 
+  /*\
+
+    class vertice, memorizza i primi vicini di un vertice in un grafo.
+    Viene memorizzata la posizione (numero unsigned)
+    nella lista (vettore STL) dei vertici.
+
+  \*/
   class Vertex {
     string           label;
     vector<unsigned> neighbors_pos;
   public:
 
-    Vertex() {}
+    Vertex() : label("") {}
 
+    // inizializza il nodo con 0 vicini e un nome
     void
     setup( string const & name ) {
       this->label = name;
       this->neighbors_pos.clear();
     }
 
+    // restituisce il grado del nodo = numero di vicini
     unsigned
     degree() const
-    { return unsigned( this->neighbors_pos.size() ) ; }
+    { return unsigned( this->neighbors_pos.size() ) ; }
 
+    // aggiunge un vicino al nodo (posizione del vicino)
     void
     add_neighbor( unsigned pos ) {
       this->neighbors_pos.push_back( pos );
     }
 
+    // aggiunge un vicino al nodo (posizione del vicino)
+    void
+    delete_neighbor( unsigned pos ) {
+      vector<unsigned>::iterator ipos = find(this->neighbors_pos.begin(),
+                                             this->neighbors_pos.end(),
+                                             pos );
+      if ( ipos != this->neighbors_pos.end() ) this->neighbors_pos.erase( ipos );
+    }
+
+    // restituisce posizione del vicini i-esimo
     unsigned
     get_neighbor( unsigned ipos ) const
-    { return this->neighbors_pos[ipos]; }
+    { return this->neighbors_pos[ipos]; }
+
+    // stampa vertice con posizione dei suoi vicini
+    void
+    dump( ostream & stream ) const {
+      stream
+        << this->label << " ->";
+      vector<unsigned>::const_iterator iv = this->neighbors_pos.begin();
+      for ( ; iv != this->neighbors_pos.end(); ++iv )
+        stream << ' ' << setw(3) << *iv;
+      stream << '\n';
+    }
 
   };
 
+  /*\
+    Vertice di un Albero, non serve memorizzare i figli
+    per gli algoritmi che intendiamo implementare
+  \*/
   class Vertex_of_a_tree {
     string   label;
     unsigned father_pos;
   public:
 
+    // inizializza il vertice con un nome e la posizione del padre
+    // in caso di root father = ~unsigned(0) ( tutti i bit a 1 )
+    void
+    setup( unsigned father ) {
+      this->father_pos = father;
+    }
+
+    // inizializza il vertice con un nome e la posizione del padre
+    // in caso di root father = ~unsigned(0) ( tutti i bit a 1 )
     void
     setup( string const & name, unsigned father ) {
       this->label      = name;
       this->father_pos = father;
     }
 
+    // inizializza il vertice con come "root" (radice) dell'albero
+    void
+    setup( string const & name ) {
+      this->label      = name;
+      this->father_pos = ~unsigned(0);
+    }
+
+    // restituisce la posizione del vertice "padre" nel grafo
     unsigned
     get_father() const
-    { return this->father_pos; }
+    { return this->father_pos; }
+
+    // true se il vertice è la root dell'albero
+    bool
+    is_root() const
+    { return this->father_pos == ~unsigned(0); }
 
   };
 
@@ -74,10 +133,43 @@ namespace GraphLib {
 
   };
 
+  /*\
+   Classe che memorizza un grafo generico nel vettore di vertici V.
+   Ogni vertice ha la lista dei suoi vicini cosicche il grafo
+   non necessita di altri dati.
+  \*/
   class Graph {
+    string         label;
     vector<Vertex> V;
   public:
 
+    // inizializza il grafo ad un grafo vuoto
+    Graph( string const & name ) : label(name) {}
+
+    // restituisce il numero totale dei vertici
+    unsigned
+    numVertex() const {
+      return unsigned( V.size() );
+    }
+
+    /*\
+      Inizializza la lista dei vertici (vettore STL)
+      ad nv vertici isolati
+    \*/
+    void
+    resize_vertex( unsigned nv ) {
+      V.clear();
+      V.resize( size_t(nv) );
+    }
+
+    // da un nome al vertice i-esimo ed eventualmente cancella i suoi vicini
+    void
+    setup_vertex( unsigned i, string const & name ) {
+      this->V[i].setup( name );
+    }
+
+    // aggiungo edge al grafo modificando i vicini
+    // dei vertici corrispondenti
     void
     add_edge( unsigned from, unsigned to ) {
       this->V[from].add_neighbor( to );
@@ -89,14 +181,68 @@ namespace GraphLib {
       this->V[to].add_neighbor( from );
     }
 
+    // aggiungo edge al grafo modificando i vicini
+    // dei vertici corrispondenti
+    void
+    delete_edge( unsigned from, unsigned to ) {
+      this->V[from].delete_neighbor( to );
+    }
+
+    void
+    delete_edge_undirected( unsigned from, unsigned to ) {
+      this->V[from].delete_neighbor( to );
+      this->V[to].delete_neighbor( from );
+    }
+
     void
     build_spanning_tree( Tree & T, unsigned v0 );
 
+    void
+    dump( ostream & stream ) const {
+      vector<Vertex>::const_iterator iV = V.begin();
+      for ( ; iV != V.end(); ++iV ) {
+        stream << "V n." << (iV-V.begin());
+        iV->dump( stream );
+      }
+    }
   };
 
   class Tree {
+    string                   label;
     vector<Vertex_of_a_tree> V;
   public:
+
+    // inizializza il grafo ad un grafo vuoto
+    Tree( string const & name ) : label(name) {}
+
+    // restituisce il numero totale dei vertici
+    unsigned
+    numVertex() const {
+      return unsigned( V.size() );
+    }
+
+    /*\
+      Inizializza la lista dei vertici (vettore STL)
+      ad nv vertici isolati
+    \*/
+    void
+    resize_vertex( unsigned nv ) {
+      V.clear();
+      V.resize( size_t(nv) );
+    }
+
+    // da un nome al vertice i-esimo ed eventualmente cancella i suoi vicini
+    void
+    setup_vertex( unsigned i, string const & name ) {
+      this->V[i].setup( name );
+    }
+
+    // aggiungo edge al grafo modificando i vicini
+    // dei vertici corrispondenti
+    void
+    setup( unsigned sun, unsigned father ) {
+      this->V[sun].setup( father );
+    }
 
   };
 
